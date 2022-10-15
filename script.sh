8f2c34f15492577deb97cd1db17b5b456e86b19f
Alia <alia01minn@gmail.com>
2022-10-15 23:32:29 +0300

    fixes and jar + script

--- script.sh
+++ script.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
+sudo rm -rf /opt/cloudphoto
+sudo mkdir -p /opt/cloudphoto
+
+sudo apt update
 sudo apt install default-jre
+sudo apt install maven
+sudo apt install zip
+
+sudo wget https://github.com/festralm/cloudphoto/archive/refs/tags/v1.0.0.zip
+sudo mv v1.0.0 /opt/cloudphoto
+sudo /opt/cloudphoto/unzip v8.1.1.zip -d /opt/cloudphoto/
+cd /opt/cloudphoto/
+sudo mv target/cloudphoto-0.0.1-SNAPSHOT.jar /opt/cloudphoto/
+
 
 file="cloudphoto"
 echo '#!/bin/bash' >> $file
 echo 'java -jar /opt/cloudphoto/cloudphoto-0.0.1-SNAPSHOT.jar "$@"' >> $file
-
 sudo chmod +x cloudphoto
-
-sudo mkdir -p /opt/cloudphoto
-
-sudo mv cloudphoto-0.0.1-SNAPSHOT.jar /opt/cloudphoto/
-
 sudo mv cloudphoto /usr/bin
